3ab7e94d60f4a6ccb1155ec38de37cbcbcb6b1fd
TlotlisoMokatiJohn <74985998+TlotlisoMokatiJohn@users.noreply.github.com>
2021-08-02 14:06:12 -0700

    main

--- src/components/Secondarybtn.js
+++ src/components/Secondarybtn.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Navigation = () => {
+const Sbutton = () => {
     return (
       <div>
         
@@ -8,4 +8,4 @@ const Navigation = () => {
     );
 }
 
-export default Navigation;
+export default Sbutton;
