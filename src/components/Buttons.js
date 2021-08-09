173cb171ae4fdab421d0c550bac33bf600f4166b
TlotlisoMokatiJohn <74985998+TlotlisoMokatiJohn@users.noreply.github.com>
2021-08-09 12:42:23 -0700

    Login form

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
-const Button = ({value,onClick}) => {
+const Button = ({value,styleClass}) => {
     return (
       <section>
-        <button className='bg-blue-500 px-5 py-2 text-white'>
+        <button className={styleClass}>
           {value}
         </button>
       </section>
