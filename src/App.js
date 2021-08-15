da8d6fa04c1e486555726a66dc7cc58be827c722
TlotlisoMokatiJohn <74985998+TlotlisoMokatiJohn@users.noreply.github.com>
2021-08-15 10:17:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navigation from './components/Navigation.js';
+import Login from './components/login';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 function App() {
 	return (
 		<Router>
