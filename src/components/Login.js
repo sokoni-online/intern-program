76946621db8a59586a7de557f022f3d85e132ef4
Tsepo <tliphoto8@gmail.com>
2021-08-30 10:38:54 -0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,45 @@
 import React from 'react';
-import PrimaryBtn from './components/PrimaryBtn'
+import Button from './Buttons';
+import {Link} from 'react-router-dom';
+function Login(){
+	return (
+		<div className="p-10 my-30">
+			<h2 className="text-center text-3xl text-blue-400 ">SIGN IN</h2>
+			<form action="">
+				<div className="flex justify-center">
+					<div className="lg:w-1/3 md:w-2/3 w-full">
+						<br />
+						<label className=" my-3 block tracking-wide text-blue-400 text-base font-bold mb-2">
+							Email
+						</label>
+						<input
+							type="email"
+							id="email"
+							placeholder="Your Email"
+							className=" bg-gray-200 appearance-none border border-transparent rounded w-full px-4 py-2
+							 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+						/> 
+						<label className=" my-3 block tracking-wide text-blue-400 text-base font-bold mb-2">
+							Password
+						</label>
+						<input type="password" id="password" className=" bg-gray-200
+                            appearance-none border border-transparent  focus:outline-none focus:ring-2 focus:ring-blue-500
+							focus:border-transparent rounded w-full px-4 py-2 text-gray-700  my-2"	
+						/>
+						<Button
+							value="Sign In"
+							styleClass="bg-blue-500 hover:bg-gray-400 hover:text-white hover:shadow-xl w-full hover:border-blue-300 hover:border 
+							px-5 py-2 text-white grid-flow-row text-lg font-bold"
+							id='btnLogin'
+						/>
+						  <Link to="/Register" className="p-4 text-blue text-blue-400 tracking-normal flex justify-center">
+							Have No Account? Signup here
+						 </Link>	
+					</div>
+				</div>
+			</form>
+		</div>
+	);
+}
 
-
-const Login = () => {
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-
-        let email = e.target.elements.email ?.value;
-        let password = e.target.elements.password ?.value;
-
-        console.log(email, password);
-    };
-
-    return (
-        <div className = 'h-screen flex bg-gray-bg1'>
-            <div className = 'w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16'>
-                <h1 className = 'text-2xl font-medium text-primary mt-4 mb-12 text-center'>Login Form</h1>
-                <form onSubmit = {handleFormSubmit}>
-                    <div className >
-                        <label htmlFor = 'email' >Email</label>
-                        <input type = 'email' className = {`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`} id = 'email' placeholder = 'Enter your email'/>
-                    </div>
-
-                    <div>
-                        <lable htmlFor = 'password'>Password</lable>
-                        <input type = 'passsword' className = {`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`} id = 'password' placeholder = 'Enter your password'/>
-                    </div>
-
-                    <div className = 'flex justify-center items-center mt-6'>
-                        <button className = {`bg-green py-2 px-4 text-sm text-white ronded-border border-green focus:outline-none focus:border-green-dark`}>Login</button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    );
-};
+export default Login;
